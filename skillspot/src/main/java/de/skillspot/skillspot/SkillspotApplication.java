50c95197aebf9c7dd67253c99929e1799a1fb305
andertben <benandert@web.de>
2025-11-23 12:03:30 +0100

    Initialisierung des Projekt-Backends

---
+++ skillspot/src/main/java/de/skillspot/skillspot/SkillspotApplication.java
@@ -0,0 +1,13 @@
+package de.skillspot.skillspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillspotApplication.class, args);
+	}
+
+}
---
+++ skillspot/src/test/java/de/skillspot/skillspot/SkillspotApplicationTests.java
@@ -0,0 +1,13 @@
+package de.skillspot.skillspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

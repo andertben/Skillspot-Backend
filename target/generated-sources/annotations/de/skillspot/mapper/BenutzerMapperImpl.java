dfd48eea9b8fcd645bdb456c3eaa60f7185875c9
bennet939 <bennet.weiss02@gmail.com>
2026-01-15 21:37:16 +0100

    kleine fixes

--- target/generated-sources/annotations/de/skillspot/mapper/BenutzerMapperImpl.java
+++ target/generated-sources/annotations/de/skillspot/mapper/BenutzerMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-13T08:30:58+0100",
+    date = "2026-01-14T21:45:46+0100",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.9 (Eclipse Adoptium)"
 )
 @Component

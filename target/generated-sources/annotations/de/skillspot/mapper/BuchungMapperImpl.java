24e38117e44c90bf8a86709d206ee7966a619528
bennet939 <bennet.weiss02@gmail.com>
2026-01-25 18:17:01 +0100

    Deploy

--- target/generated-sources/annotations/de/skillspot/mapper/BuchungMapperImpl.java
+++ target/generated-sources/annotations/de/skillspot/mapper/BuchungMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-25T18:14:29+0100",
+    date = "2026-01-25T18:16:30+0100",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Homebrew)"
 )
 @Component
